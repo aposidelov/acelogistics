951153d9864aafa1491736233b20f19fab261c82
alex posidelov <alex.posidelov@gmail.com>
2016-04-18 13:02:06 +0200

    client job rate update

--- sites/all/modules/custom/modules/ace_client_job/ace_client_job.js
+++ sites/all/modules/custom/modules/ace_client_job/ace_client_job.js
@@ -2,10 +2,10 @@ $(document).ready(function(){
 	$('#edit-field-job-client-name-0-value').change(function() {
 		//console.log($(this).val());
 		var client_name = $(this).val();
-		$.getJSON('/admin/ajax/client-rate/' + client_name, function(data) {
+		$.getJSON('/admin/ajax/client-rate/' + client_name, function(data) {			
 			$('#edit-field-client-2h-default-0-value').val(data.hours_2);
 			$('#edit-field-client-3h-default-0-value').val(data.hours_3);
-			alert('Client 2h, 3h charges have been added for this job (see below)');			
+			$('#edit-field-client-hourly-rate-default-value').val(data.hourly_rate);
 		});
 	});
 });
