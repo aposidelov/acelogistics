f804ae98fbb51305f50d1ad483493256565709fe
alex posidelov <alex.posidelov@gmail.com>
2016-05-05 09:25:47 +0200

    job page - modals for client contact & venue add

--- sites/all/modules/custom/modules/op_custom/js/job.js
+++ sites/all/modules/custom/modules/op_custom/js/job.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){	
-	$('#edit-field-job-client-name-0-value').change(function() {
-		//console.log($(this).val());
+	$('#edit-field-job-client-name-0-value').change(function() {		
 		var client_name = $(this).val();
-		$.getJSON('/admin/ajax/client-account/' + client_name, function(data) {
+		//client_name = $.trim(client_name);		
+		console.log(escape(client_name));		
+		$.getJSON('/admin/ajax/client-account/-?client_name=' + escape(client_name), function(data) {
+			console.log(data);
+			if (data.client_nid) {								
+				var href = '/ace_modals/nojs/client_contact_add/' + data.client_nid;
+				$('.client-contact-add-container a').attr('href', href);
+				$('.client-contact-add-container').show();
+			} else {
+				$('.client-contact-add-container').hide();
+			}
 			if (!data.status) {				
 				alert('Client Does not have an account');
 			}
