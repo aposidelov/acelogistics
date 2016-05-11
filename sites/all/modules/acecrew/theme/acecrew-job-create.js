8c3b661e731287cb8ebfa7d4774c58c48ecd5db3
alex posidelov <alex.posidelov@gmail.com>
2016-05-11 16:48:37 +0200

    add second contact box for job

--- sites/all/modules/acecrew/theme/acecrew-job-create.js
+++ sites/all/modules/acecrew/theme/acecrew-job-create.js
@@ -23,13 +23,41 @@ function fill_client_contacts_selectbox(selected_contact){
 function clear_client_contacts_selectbox(){
     var selected_contact = $("#edit-field-job-client-contact-value option:selected").val();
     fill_client_contacts_selectbox(selected_contact);
-
 }
 
-$(document).ready(function() {
-    clear_client_contacts_selectbox();
+function fill_client_contacts_second_selectbox(selected_contact){
+    $("#edit-field-job-client-contact-second-value").html('');
+    var client_name = $('#edit-field-job-client-name-0-value').val();
 
-    $("#edit-field-job-client-name-0-value").change(function(){
+    client_name = escape(encodeURIComponent(client_name));
+
+    $.get(Drupal.settings.basePath + "acecrew/ajax/client_contacts/" + client_name, function(data){
+        var result = Drupal.parseJson(data);
+
+        $.each(result, function(val, text) {
+
+            if(selected_contact == val) {
+                $("#edit-field-job-client-contact-second-value").append($('<option selected="selected"></option>').val(val).html(text));
+            } else {
+                $("#edit-field-job-client-contact-second-value").append($('<option></option>').val(val).html(text));
+            }
+
+        });
+    });
+}
+
+function clear_client_contacts_second_selectbox() {
+    var selected_contact = $("#edit-field-job-client-contact-second-value option:selected").val();
+
+    fill_client_contacts_second_selectbox(selected_contact);
+}
+
+$(document).ready(function() {    
+    clear_client_contacts_selectbox();
+    clear_client_contacts_second_selectbox();
+
+    $("#edit-field-job-client-name-0-value").change(function() {
         fill_client_contacts_selectbox();
+        fill_client_contacts_second_selectbox();
     });
 });
--- sites/all/modules/custom/modules/op_custom/js/job.js
+++ sites/all/modules/custom/modules/op_custom/js/job.js
@@ -1,20 +1,33 @@
 $(document).ready(function(){	
 	$('#edit-field-job-client-name-0-value').change(function() {		
 		var client_name = $(this).val();
+		var second_contact = $('#second-contact-label').text();
 		//client_name = $.trim(client_name);		
 		console.log(escape(client_name));		
-		$.getJSON('/admin/ajax/client-account/-?client_name=' + escape(client_name), function(data) {
-			console.log(data);
-			if (data.client_nid) {								
-				var href = '/ace_modals/nojs/client_contact_add/' + data.client_nid;
-				$('.client-contact-add-container a').attr('href', href);
-				$('.client-contact-add-container').show();
-			} else {
-				$('.client-contact-add-container').hide();
-			}
-			if (!data.status) {				
-				alert('Client Does not have an account');
-			}
-		});
+		update_client_contact_link(client_name, second_contact, true);
+	});
+
+	$('#edit-field-job-client-contact-second-value').change(function() {
+		var client_name = $('#edit-field-job-client-name-0-value').val();
+		var contact = $(this).find('option:selected').text();
+		$('#second-contact-label').text(contact);
+
+		update_client_contact_link(client_name, contact, false);
 	});
 });
+
+function update_client_contact_link(client_name, second_contact, show_alert) {
+	$.getJSON('/admin/ajax/client-account/-?client_name=' + escape(client_name), function(data) {		
+		if (data.client_nid) {
+			second_contact = !show_alert ? second_contact : '-';
+			var href = '/ace_modals/nojs/client_contact_add/' + data.client_nid + '/' + escape(second_contact);
+			$('.client-contact-add-container a').attr('href', href);
+			$('.client-contact-add-container').show();
+		} else {
+			$('.client-contact-add-container').hide();
+		}
+		if (show_alert && !data.status) {				
+			alert('Client Does not have an account');
+		}
+	});
+}
